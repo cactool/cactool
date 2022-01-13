628ffbc2712d0e017fab84f096ed4a2958d847bf
Sam Ezeh <sam.z.ezeh@gmail.com>
2022-01-13 21:28:41 +0000

    Add user table

--- app/__init__.py
+++ app/__init__.py
@@ -25,4 +25,7 @@ def signup():
             pass
 
 if __name__ == "__main__":
-    db.init_app()
+    db.init_app(app)
+    with app.app_context():
+        db.create_all()
+    app.run(debug=True)
--- app/database.py
+++ app/database.py
@@ -1,3 +1,9 @@
 from flask_sqlalchemy import SQLAlchemy
  
 db = SQLAlchemy()
+
+class User(db.Model):
+    ID = db.Column(db.String(512), unique=True, primary_key=True)
+    admin = db.Column(db.Boolean)
+    username = db.Column(db.String(20), unique=True)
+    password = db.Column(db.String(1024))
