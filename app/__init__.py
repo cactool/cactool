ae6bfeae75f59c7360681970003761e343377ef0
Sam Ezeh <sam.z.ezeh@gmail.com>
2022-01-13 21:18:24 +0000

    Basic signup login start

--- app/__init__.py
+++ app/__init__.py
@@ -1,4 +1,6 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
+from passlib.hash import pbkdf2_sha256
+from database import db
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:////tmp/test.db'
@@ -11,9 +13,16 @@ def index():
 def login():
     return render_template("login.html")
 
-@app.route("/signup")
+@app.route("/signup", methods=["POST", "GET"])
 def signup():
-    return render_template("signup.html")
+    if request.method == "GET":
+        return render_template("signup.html")
+    if request.method == "POST":
+        username = request.form.get("username")
+        password = request.form.get("password")
+        
+        if username and password:
+            pass
 
 if __name__ == "__main__":
     db.init_app()
